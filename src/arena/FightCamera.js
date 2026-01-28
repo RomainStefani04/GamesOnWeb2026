f3744d61f41f8aee25d7c26ba4340632bd4c9924
guillaume <faureliffran.guillaume@gmail.com>
2026-01-28 17:16:01 +0100

    Ground on arena

--- src/arena/FightCamera.js
+++ src/arena/FightCamera.js
@@ -9,6 +9,7 @@ export class FightCamera {
 
     init() {
         this.camera = new BABYLON.ArcRotateCamera("camera1", 0, 0, 0, new BABYLON.Vector3(0, 0, 0), this.scene);
+        console.log("Positionnement de la caméra de combat: "+this.camera.position);
         var angle = 20;
         this.camera.lowerAlphaLimit = angle
         this.camera.upperAlphaLimit = angle
@@ -17,8 +18,6 @@ export class FightCamera {
         this.camera.panningDistanceLimit = 10
         this.camera.lowerRadiusLimit = angle
         this.camera.upperRadiusLimit = angle
-        this.camera.setTarget(BABYLON.Vector3.Zero());
         this.camera.attachControl(this.scene.getEngine().getRenderingCanvas(), true);
     }
-
-}   
+}
